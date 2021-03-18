f7a1fd66c0f61702bb4f8d953d5d475cc8ff478a
cankush625 <cankush625@gmail.com>
2021-03-18 17:11:37 +0530

    Code for configuring Hadoop Cluster
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ hadoopCluster/hadoop.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+from jinja2 import Environment, FileSystemLoader
+import subprocess
+import cgi
+
+print("content-type: text/html")
+print()
+
+mydata = cgi.FieldStorage()
+
+namenode_ip = mydata.getvalue("namenode_ip")
+namenode_port = mydata.getvalue("namenode_port")
+namenode_directory = mydata.getvalue("namenode_directory")
+datanode_ip = mydata.getvalue("datanode_ip")
+datanode_directory = mydata.getvalue("datanode_directory")
+
+def installtionScript(nodeType,directoryPath):
+    file_loader = FileSystemLoader('hadoopCluster/templates')
+    env = Environment(loader=file_loader)
+    template = env.get_template('installationScript.sh.j2')
+    output = template.render(nodeType = nodeType , directoryPath = directoryPath)
+    file = open("./hadoopCluster/temp/installationScript.sh", "w")
+    file.write("%s" %(output))
+    file.close()
+
+def hdfsSite(nodeType,directoryPath):
+    file_loader = FileSystemLoader('hadoopCluster/templates')
+    env = Environment(loader=file_loader)
+    template = env.get_template('hdfs-site.xml.j2')
+    output = template.render(nodeType = nodeType , directoryPath = directoryPath)
+    file = open("./hadoopCluster/temp/hdfs-site.xml", "w")
+    file.write("%s" %(output))
+    file.close()
+
+def coreSite(nodeIp,nodePort):
+    file_loader = FileSystemLoader('hadoopCluster/templates')
+    env = Environment(loader=file_loader)
+    template = env.get_template('core-site.xml.j2')
+    output = template.render(IP = nodeIp , port = nodePort)
+    file = open("./hadoopCluster/temp/core-site.xml", "w")
+    file.write("%s" %(output))
+    file.close()
+
+def copyTemplate(nodeIP):
+    subprocess.run(f'scp ./hadoopCluster/temp/hdfs-site.xml root@{nodeIP}:/root/hdfs-site.xml',shell=True)
+    subprocess.run(f'scp ./hadoopCluster/temp/core-site.xml root@{nodeIP}:/root/core-site.xml',shell=True)
+
+def nameNode(nameNodeIP):
+    nameNodeDirectory = namenode_directory
+    nameNodePort = namenode_port
+    hdfsSite('name',f'/root/{nameNodeDirectory}')
+    coreSite(nameNodeIP,nameNodePort)
+    copyTemplate(nameNodeIP)
+    installtionScript('name',nameNodeDirectory)
+    subprocess.run(f"ssh root@{nameNodeIP} 'bash -s' < ./hadoopCluster/temp/installationScript.sh",shell=True)
+    return nameNodePort
+
+def dataNode(dataNodeIP,nameNodeIP,nameNodePort):
+    dataNodeDirectory = datanode_directory
+    hdfsSite('data',f'/root/{dataNodeDirectory}')
+    coreSite(nameNodeIP,nameNodePort)
+    copyTemplate(dataNodeIP)
+    installtionScript('data',dataNodeDirectory)
+    subprocess.run(f"ssh root@{dataNodeIP} 'bash -s' < ./hadoopCluster/temp/installationScript.sh",shell=True)
+
+def configure():
+    nameNodeIP = namenode_ip
+    dataNodeIP = datanode_ip
+    nameNodePort = nameNode(nameNodeIP)
+    dataNode(dataNodeIP,nameNodeIP,nameNodePort)
+    print("HADOOP CLUSTER SUCCESS")
+
+configure()
---
+++ hadoopCluster/temp/installationScript.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+systemctl stop firewalld
+systemctl disable firewalld
+mkdir /data
+yum install python36 -y
+pip3 install gdown
+gdown --id 17UWQNVdBdGlyualwWX4Cc96KyZhD-lxz
+gdown --id 1541gbFeGZZJ5k9Qx65D04lpeNBw87rM5
+rpm -i /root/jdk-8u171-linux-x64.rpm
+rpm -i /root/hadoop-1.2.1-1.x86_64.rpm --force
+cp /root/hdfs-site.xml /etc/hadoop/
+cp /root/core-site.xml /etc/hadoop/
+
+hadoop-daemon.sh start datanode
