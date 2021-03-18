02060666f6c5d76babaf66b53e4e8dda9db312d7
cankush625 <cankush625@gmail.com>
2021-03-18 13:18:38 +0530

    Code for configuring httpd webserver on docker
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- docker.py
+++ docker.py
@@ -8,9 +8,18 @@ print()
 
 mydata = cgi.FieldStorage()
 
-if mydata.getvalue("x") == "python":
+task = mydata.getvalue("x")
+
+if task == "python":
     confPython()
     print("SUCCESS")
+elif task == "httpd":
+    image_name = mydata.getvalue("image_name")
+    confHttpd(image_name)
+    print("HTTPD SUCCESS")
 
 def confPython():
     sp.run(f"sudo docker run centos /bin/bash -c 'dnf install python3 -y; echo \"print('hello')\" > aa.py; python3 aa.py'",shell=True)
+
+def confHttpd(image_name):
+    sp.run(f"sudo docker run {image_name} /bin/bash -c 'dnf install httpd -y; /usr/sbin/httpd'", shell=True)
