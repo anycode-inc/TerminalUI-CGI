4936895228894161d131f628c1c350e120dbb7c7
cankush625 <cankush625@gmail.com>
2021-03-19 23:36:54 +0530

    Bug Fixed
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- aws/ec2_instance.py
+++ aws/ec2_instance.py
@@ -1,4 +1,4 @@
 import subprocess as sp
 
 def launchInstance(image_id, instance_type, count, subnet_id, security_group_id, key_name):
-    sp.run("aws ec2 run-instances --image-id {0} --instance-type {1} --count {2} --subnet-id {3} --security-group-ids {4} --key-name {5} --user-data file://aws/ud.txt".format(image_id, instance_type, count, subnet_id, security_group_id, key_name), shell=True)
+    sp.getoutput("aws ec2 run-instances --image-id {0} --instance-type {1} --count {2} --subnet-id {3} --security-group-ids {4} --key-name {5} --user-data file://aws/ud.txt".format(image_id, instance_type, count, subnet_id, security_group_id, key_name))
