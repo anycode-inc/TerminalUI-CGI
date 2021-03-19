020f7e81751ecedf10e4cd9859418d7a7fea9e0c
cankush625 <cankush625@gmail.com>
2021-03-20 00:16:31 +0530

    Sudo privilege to run the aws command is added
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- aws/s3.py
+++ aws/s3.py
@@ -1,4 +1,4 @@
 import subprocess as sp
 
 def uploadFileToBucket(localFileLocation, s3BucketLocation, acl):
-    sp.getoutput("aws s3 cp {0} {1} --acl {2}".format(localFileLocation, s3BucketLocation, acl))
+    sp.getoutput("sudo aws s3 cp {0} {1} --acl {2}".format(localFileLocation, s3BucketLocation, acl))
